ba93bfe210eba849b593eb853844d49e44bccf67
SharadPatel1993 <sharad182@hotmail.com>
2017-05-02 11:21:02 +0100

    Added generated steps

---
+++ features/step_definitions/front_page_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the Deliveroo homepage$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I enter a valid postcode$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click the find food button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it should show a list of restuarants$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
